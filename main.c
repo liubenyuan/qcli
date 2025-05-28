114a41e15a688080869e6683d494f0f1210ccfee
byliu <liubenyuan@gmail.com>
2025-05-28 13:31:54 +0800

    add qcli_add examples

--- main.c
+++ main.c
@@ -24,8 +24,44 @@ static QCliObj cli;
    cmd2 <arg>
    cmd2 test exec_str_test
  */
+
+static QCliCmd _cmd1, _cmd2, _cmd3;
+
+int _cmd1_cb(int argc, char **argv) {
+  QSH("cmd1 called\r\n");
+  return 0;
+}
+
+int _cmd2_cb(int argc, char **argv) {
+  QSH("cmd2 %s\r\n", argv[1]);
+  return 0;
+}
+
+int _cmd3_cb(int argc, char **argv) {
+  if (argc < 3) {
+    QSH("cmd3 requires two arguments\r\n");
+    return 1;
+  }
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+  QSH("cmd3: %d + %d = %d\r\n", a, b, a + b);
+  return 0;
+}
+
 int main() {
   qcli_init(&cli, printf);
+
+  qcli_add(&cli, &_cmd1, "cmd1", _cmd1_cb, "cmd1");
+  qcli_add(&cli, &_cmd2, "cmd2", _cmd2_cb, "echo: cmd2 <arg>");
+  qcli_add(&cli, &_cmd3, "cmd3", _cmd3_cb, "adder: cmd3 <arg1> <arg2>");
+  int ret;
+  ret = qcli_exec_str(&cli, "cmd1");
+  QSH("cmd1_exec: %d\r\n", ret);
+  ret = qcli_exec_str(&cli, "cmd2 ddd");
+  QSH("cmd2_exec1: %d\r\n", ret);
+  ret = qcli_exec_str(&cli, "cmd3 2 10");
+  QSH("cmd2_exec2: %d\r\n", ret);
+
   char ch;
   for (;;) {
     if (system("stty raw -echo") < 0) {
